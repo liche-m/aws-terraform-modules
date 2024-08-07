830ca6edb610f9cc2cbdbc93bd7290d1091e9197
liche-m <moodleyliche@gmail.com>
2024-08-07 14:54:36 +0200

    Initial Commit

---
+++ versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 1.2"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.40"
+    }
+  }
+}
